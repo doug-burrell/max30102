117adcb9d3054f538834017bea2d6c518c6c0da7
Fernanda <espinoza.fernandan@gmail.com>
2022-05-25 16:29:59 -0700

    quite parser time

--- main.py
+++ main.py
@@ -2,33 +2,23 @@ from heartrate_monitor import HeartRateMonitor
 import time
 import argparse
 import dash
-
-from dash import html
-from dash import dcc
-from dash.dependencies import Input, Output
 import pandas as pd
-import plotly.express as px
 import matplotlib.pyplot as plt
 
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
+t = 30
 
 parser = argparse.ArgumentParser(description="Read and print data from MAX30102")
 parser.add_argument("-r", "--raw", action="store_true",
                     help="print raw data instead of calculation result")
-parser.add_argument("-t", "--time", type=int, default=30,
-                    help="duration in seconds to read from sensor, default 30")
 args = parser.parse_args()
 
 print('sensor starting...')
+hrm = HeartRateMonitor()
 hrm = HeartRateMonitor(print_raw=args.raw, print_result=(not args.raw))
 hrm.start_sensor()
 
 try:
-    time.sleep(args.time)
+    time.sleep(t)
 except KeyboardInterrupt:
     print('keyboard interrupt detected, exiting...')
 
@@ -39,7 +29,7 @@ print('sensor stopped!')
 ## plot
 # df_raw = pd.read_csv('raw_values.csv')
 # plt.plot(df_raw)
-df_final = pd.read_csv('final_values.csv')
-plt.plot(df_final)
+# df_final = pd.read_csv('final_values.csv')
+# plt.plot(df_final)
 
-plt.show()
+# plt.show()
